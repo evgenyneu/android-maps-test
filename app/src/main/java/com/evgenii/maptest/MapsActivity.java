6d447767e64dbdc682cb0e544f22b424e7e0a6f6
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-12-23 18:45:53 +1100

    Show marker

--- app/src/main/java/com/evgenii/maptest/MapsActivity.java
+++ app/src/main/java/com/evgenii/maptest/MapsActivity.java
@@ -2,22 +2,37 @@ package com.evgenii.maptest;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Build;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+        com.google.android.gms.location.LocationListener {
 
     private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private LocationRequest mLocationRequest;
 
     private static final String[] INITIAL_PERMS={
             Manifest.permission.ACCESS_FINE_LOCATION,
@@ -29,6 +44,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -40,8 +56,43 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 requestPermissions(INITIAL_PERMS, LOCALTION_REQUEST);
             }
         }
+
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
     }
 
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        zoomToCurrentLocation();
+        createLocationRequest();
+        startLocationUpdates();
+    }
+
+    @Override
+    public void onConnectionSuspended(int var1) {
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult var1) {
+
+    }
 
     boolean hasLocationPermission() {
         return ContextCompat.checkSelfPermission(this,
@@ -56,6 +107,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             case LOCALTION_REQUEST:
                 if (hasLocationPermission()) {
                     mMap.setMyLocationEnabled(true);
+                    mMap.getUiSettings().setMyLocationButtonEnabled(false);
                 }
                 else {
                     Toast.makeText(this, "Location denied", Toast.LENGTH_LONG).show();
@@ -78,10 +130,52 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(false);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        zoomToCurrentLocation();
+    }
+
+    private void zoomToCurrentLocation() {
+        if (mLastLocation == null) { return; }
+
+        LatLng latLng = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17));
+
+        createShapes();
+    }
+
+    private void createShapes() {
+        LatLng latLng = new LatLng(-37.859621, 144.977781);
+
+        // Instantiates a new CircleOptions object and defines the center and radius
+        CircleOptions circleOptions = new CircleOptions()
+                .center(latLng)
+                .fillColor(Color.parseColor("#33A4AFFF"))
+                .strokeColor(Color.parseColor("#A4AFFF"))
+                .strokeWidth(3)
+                .radius(90); // In meters
+
+        // Add a marker
+        mMap.addMarker(new MarkerOptions().position(latLng).title("Walk to circle"));
+
+        // Get back the mutable Circle
+        Circle circle = mMap.addCircle(circleOptions);
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    protected void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLastLocation = location;
     }
 }
