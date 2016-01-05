de81fbd993a2ab1cc60b4098244db8f9ee68a406
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2016-01-05 18:20:59 +1100

    Checking for Google Play services

--- app/src/main/java/com/evgenii/maptest/MainActivity.java
+++ app/src/main/java/com/evgenii/maptest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.evgenii.maptest;
 
+import android.app.Dialog;
 import android.app.Fragment;
 import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
@@ -10,8 +11,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.evgenii.maptest.Utils.WalkCameraDistance;
+import com.evgenii.maptest.Utils.WalkGooglePlayServices;
 import com.evgenii.maptest.Utils.WalkLocation;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,16 +28,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        WalkGoogleApiClient.getInstance().create();
-
-        try {
-            int v = getPackageManager().getPackageInfo("com.google.android.gms", 0 ).versionCode;
-            Log.d("ii", "Google Play Services version: " + v);
-        } catch (PackageManager.NameNotFoundException e) {
-            Log.d("ii", "Google Play Services NOT FOUND" + e.getMessage());
-            e.printStackTrace();
-        }
 
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
@@ -48,7 +42,28 @@ public class MainActivity extends AppCompatActivity {
         registerApiClientCallback();
         registerLocationPermissionCallback();
         WalkLocationPermissions.getInstance().requestLocationPermissionIfNotGranted(this);
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        startGooglePlayServices();
+    }
+
+    private void startGooglePlayServices() {
+        WalkGooglePlayServices.logGooglePlayServicesVersion(this);
+
+        int resultCode = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this);
+
+        if (resultCode == ConnectionResult.SUCCESS) {
+            WalkGoogleApiClient.getInstance().create();
+        } else if (resultCode == ConnectionResult.SERVICE_MISSING ||
+                resultCode == ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED ||
+                resultCode == ConnectionResult.SERVICE_DISABLED) {
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(this, resultCode, 1);
+            dialog.show();
+        }
     }
 
     private void registerApiClientCallback() {
